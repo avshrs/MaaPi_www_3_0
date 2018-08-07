485dee7d62fc48009237ef15c4a4012e2591ac26
avshrs <sebastian.antosiak@gmail.com>
2018-08-07 23:01:01 +0200

    autocommit on change

--- maapi/views_Dev_Charts.py
+++ maapi/views_Dev_Charts.py
@@ -1,5 +1,5 @@
 from .models import Devices, Groups, DevValues
-from django.shortcuts import  render
+from django.shortcuts import render
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 import calendar
@@ -8,6 +8,7 @@ from itertools import chain
 from django.db import connection
 import time
 
+
 def is_number(s):
     try:
         float(s)
@@ -15,23 +16,30 @@ def is_number(s):
     except ValueError:
         return False
 
-def devCharts(request,pk,acc,date_from,date_to):
+
+def devCharts(request, pk, acc, date_from, date_to):
     groupname = []
     grouplist = []
-    list_of_devices = Devices.objects.all().filter(dev_status = True).filter(dev_hidden = False).order_by('dev_user_id')
+    list_of_devices = Devices.objects.values('dev_id', 'dev_user_name').filter(
+        dev_status=True).filter(dev_hidden=False).order_by('dev_user_id')
     dataName = []
     acc2 = 1
     datetime_format = "%Y-%m-%d %H:%M:%S"
     if date_from == '-day':
-        date_from_space = datetime.now().replace(microsecond=0) - timedelta(days=1)
+        date_from_space = datetime.now().replace(microsecond=0) - timedelta(
+            days=1)
     elif date_from == '-hour':
-        date_from_space = datetime.now().replace(microsecond=0) - timedelta(hours=1)
+        date_from_space = datetime.now().replace(microsecond=0) - timedelta(
+            hours=1)
     elif date_from == '-month':
-        date_from_space = datetime.now().replace(microsecond=0) - timedelta(days=30)
+        date_from_space = datetime.now().replace(microsecond=0) - timedelta(
+            days=30)
     elif date_from == '-week':
-        date_from_space = datetime.now().replace(microsecond=0) - timedelta(days=7)
+        date_from_space = datetime.now().replace(microsecond=0) - timedelta(
+            days=7)
     elif date_from == '-year':
-        date_from_space = datetime.now().replace(microsecond=0) - timedelta(days=365)
+        date_from_space = datetime.now().replace(microsecond=0) - timedelta(
+            days=365)
     else:
         date_from_space = date_from.encode('ascii')
 
@@ -40,12 +48,12 @@ def devCharts(request,pk,acc,date_from,date_to):
         hour_to_html = "now"
     else:
         date_to_space = date_to.encode('ascii')
-        hour_to_html = datetime.strptime(str(date_to_space), datetime_format).hour
+        hour_to_html = datetime.strptime(str(date_to_space),
+                                         datetime_format).hour
 
     a = datetime.strptime(str(date_from_space), datetime_format)
     b = datetime.strptime(str(date_to_space), datetime_format)
     delta_date = (b - a).days
-
 
     date_to_html = datetime.strptime(str(date_to_space), datetime_format)
     date_from_html = datetime.strptime(str(date_from_space), datetime_format)
@@ -55,30 +63,37 @@ def devCharts(request,pk,acc,date_from,date_to):
         if delta_date != 0:
             acc2 = 2 * delta_date
     print acc2
-    graph_param = {'pk':pk,'acc':acc2,'date_from':date_from_space,'date_to':date_to_space}
+    graph_param = {
+        'pk': pk,
+        'acc': acc2,
+        'date_from': date_from_space,
+        'date_to': date_to_space
+    }
 
     if is_number(pk) == True:
-        dataName = Devices.objects.filter(dev_id = pk)
+        dataName = Devices.objects.filter(dev_id=pk)
         groupname = 'null'
-        dev_rom_id = Devices.objects.filter(dev_id=pk).values_list('dev_rom_id', flat=True)[0]
-        grouplist=[pk]
+        dev_rom_id = Devices.objects.filter(dev_id=pk).values_list(
+            'dev_rom_id', flat=True)[0]
+        grouplist = [pk]
     else:
-        ff=[]
+        ff = []
         groupname = pk
         grouplist = pk.split(",")
     date_time = datetime.now()
-    return render(request, '1/_Charts.html', {
-                                              'date_to_html': date_to_html,
-                                              'hour_to_html':hour_to_html,
-                                              'date_from_html': date_from_html,
-                                              'hour_from_html':hour_from_html,
-                                              'dataName':dataName,
-                                              'list_of_devices':list_of_devices,
-                                              'chartACC':acc ,
-                                              'date_time':date_time,
-                                              'loop':range(30),
-                                              'loop_hour':range(0,23),
-                                              'grouplist':grouplist,
-                                              'groupname':groupname,
-                                              'graph_param':graph_param,
-                                              })
+    return render(
+        request, '1/_Charts.html', {
+            'date_to_html': date_to_html,
+            'hour_to_html': hour_to_html,
+            'date_from_html': date_from_html,
+            'hour_from_html': hour_from_html,
+            'dataName': dataName,
+            'list_of_devices': list_of_devices,
+            'chartACC': acc,
+            'date_time': date_time,
+            'loop': range(30),
+            'loop_hour': range(0, 23),
+            'grouplist': grouplist,
+            'groupname': groupname,
+            'graph_param': graph_param,
+        })
