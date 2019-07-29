5ad0a1d16043aa3592e5dbc3963b515140a2c62a
avshrs <avshrs@gmail.com>
2019-07-29 12:36:38 +0200

    clean up

--- maapi/py_script/11_05_2018_old/back/test.py
+++
@@ -1,4 +0,0 @@
-from MaaPi_DB_connection import MaaPiDBConnection
-
-d=MaaPiDBConnection().order_by('dev_id').filters_eq(dev_id=1).get_table("devices")
-print(d[1]['dev_value'],d[1]['dev_value_old'],d[1]['dev_user_id'],d[1]['dev_user_name'])
--- maapi/py_script/11_05_2018_old/test.py
+++
@@ -1,5 +0,0 @@
-
-from lib.lib_maapi_gy_273_i2c_ft232_v2  import class_get_values as bh
-
-
-bh()
